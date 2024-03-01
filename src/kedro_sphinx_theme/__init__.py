f9a406cbab580e8f7c2acc09eb6c96bf3407adb5
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2024-03-01 18:50:22 +0100

    Basic Sphinx theme
    Signed-off-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>

--- src/kedro_sphinx_theme/__init__.py
+++ src/kedro_sphinx_theme/__init__.py
@@ -1,3 +1,16 @@
 """Sphinx theme for the Kedro ecosystem."""
 
 from __future__ import annotations
+
+import os
+import typing as t
+
+if t.TYPE_CHECKING:
+    from sphinx.application import Sphinx
+
+__version__ = "2024.3.0"
+
+
+def setup(app: Sphinx):
+    """Register theme."""
+    app.add_html_theme("kedro-sphinx-theme", os.path.abspath(os.path.dirname(__file__)))
